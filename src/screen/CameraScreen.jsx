f0dc1853d8e2a42661ba38780e55ff6e19e46b9a
hansol <stellanew21@gmail.com>
2022-06-11 22:32:53 +0900

    Feat: post new Video, change screen UI and add pick video from gallery - 동영상 촬영, 녹화 및 비디오 게시 기능 추가 - 일부 카메라, 녹화본 재생 등 UI 변경 - 갤러리에서 비디오 영상 업로드 기능

---
+++ src/screen/CameraScreen.jsx
@@ -0,0 +1,222 @@
+import React, { useState, useEffect, useRef } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import { Camera } from "expo-camera";
+import * as ImagePicker from "expo-image-picker";
+import * as MediaLibrary from "expo-media-library";
+import { useIsFocused } from "@react-navigation/core";
+import { Feather } from "@expo/vector-icons";
+
+function CameraScreen({ navigation }) {
+  const [hasAudioPermission, setHasAudioPermission] = useState(null);
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [hasGalleryPermissions, setHasGalleryPermissions] = useState(null);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const [cameraFlash, setCameraFlash] = useState(
+    Camera.Constants.FlashMode.off,
+  );
+  const cameraRef = useRef(null);
+
+  const [galleryItems, setGalleryItems] = useState([]);
+  const [isCameraReady, setIsCameraReady] = useState(false);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    (async () => {
+      const cameraPermissionStatus =
+        await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraPermissionStatus.status === "granted");
+
+      const audioPermissionStatus =
+        await Camera.requestMicrophonePermissionsAsync();
+      setHasAudioPermission(audioPermissionStatus.status === "granted");
+
+      const galleryStatus =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasGalleryPermissions(galleryStatus.status === "granted");
+
+      if (galleryStatus.status === "granted") {
+        const userGalleryMedia = await MediaLibrary.getAssetsAsync({
+          sortBy: ["creationTime"],
+          mediaType: ["video"],
+        });
+        setGalleryItems(userGalleryMedia.assets);
+      }
+    })();
+  }, []);
+
+  const recordVideo = async () => {
+    if (cameraRef) {
+      try {
+        const options = {
+          maxDuration: 3,
+          quality: Camera.Constants.VideoQuality["480"],
+        };
+
+        const videoRecordPromise = cameraRef.current.recordAsync(options);
+
+        if (videoRecordPromise) {
+          const liveVideo = await videoRecordPromise;
+          navigation.navigate("VideoResult", { liveVideo });
+        }
+      } catch (error) {
+        console.warn(error);
+      }
+    }
+  };
+  const stopVideo = async () => {
+    if (cameraRef) {
+      cameraRef.current.stopRecording();
+    }
+  };
+
+  const pickFromGallery = async () => {
+    const galleryVideo = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 1,
+    });
+    if (!galleryVideo.cancelled) {
+      navigation.navigate("VideoResult", { galleryVideo });
+    }
+  };
+
+  if (!hasCameraPermission || !hasAudioPermission || !hasGalleryPermissions) {
+    return <View></View>;
+  }
+
+  return (
+    <View style={styles.container}>
+      {isFocused ? (
+        <Camera
+          ref={cameraRef}
+          style={styles.camera}
+          ratio={"16:9"}
+          type={cameraType}
+          flashMode={cameraFlash}
+          onCameraReady={() => setIsCameraReady(true)}
+        />
+      ) : null}
+
+      <View style={styles.sideBarContainer}>
+        <TouchableOpacity
+          style={styles.sideBarButton}
+          onPress={() =>
+            setCameraType(
+              cameraType === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back,
+            )
+          }
+        >
+          <Feather name="refresh-ccw" size={24} color={"white"} />
+          <Text style={styles.iconText}>Flip</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.sideBarButton}
+          onPress={() =>
+            setCameraFlash(
+              cameraFlash === Camera.Constants.FlashMode.off
+                ? Camera.Constants.FlashMode.torch
+                : Camera.Constants.FlashMode.off,
+            )
+          }
+        >
+          <Feather name="zap" size={24} color={"white"} />
+          <Text style={styles.iconText}>Flash</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.bottomBarContainer}>
+        <View style={{ flex: 1 }}></View>
+
+        <View style={styles.recordButtonContainer}>
+          <TouchableOpacity
+            disabled={!isCameraReady}
+            onLongPress={() => recordVideo()}
+            onPressOut={() => stopVideo()}
+            style={styles.recordButton}
+          />
+        </View>
+        <View style={{ flex: 1 }}>
+          <TouchableOpacity
+            onPress={() => pickFromGallery()}
+            style={styles.galleryButton}
+          >
+            {galleryItems[0] === undefined ? (
+              <></>
+            ) : (
+              <Image
+                style={styles.galleryButtonImage}
+                source={{ uri: galleryItems[0].uri }}
+              />
+            )}
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 30,
+  },
+  camera: {
+    flex: 1,
+    backgroundColor: "black",
+    aspectRatio: 9 / 16,
+  },
+  bottomBarContainer: {
+    alignItems: "center",
+    position: "absolute",
+    bottom: 0,
+    flexDirection: "row",
+    marginBottom: 30,
+  },
+  recordButtonContainer: {
+    flex: 1,
+    marginHorizontal: 30,
+  },
+  recordButton: {
+    borderWidth: 8,
+    borderColor: "#ff404087",
+    backgroundColor: "#ff4040",
+    borderRadius: 100,
+    height: 80,
+    width: 80,
+    alignSelf: "center",
+  },
+  galleryButton: {
+    borderWidth: 2,
+    borderColor: "white",
+    borderRadius: 10,
+    overFlow: "hidden",
+    height: 60,
+    width: 60,
+  },
+  galleryButtonImage: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
+  },
+  sideBarContainer: {
+    top: 60,
+    right: 0,
+    marginHorizontal: 20,
+    position: "absolute",
+  },
+  iconText: {
+    color: "white",
+    fontSize: 12,
+    marginTop: 5,
+  },
+  sideBarButton: {
+    alignItems: "center",
+    marginBottom: 25,
+  },
+});
+
+export default CameraScreen;
---
+++ src/screen/VideoResultScreen.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useRef } from "react";
+import { Video } from "expo-av";
+import { StyleSheet, View, Button } from "react-native";
+
+function VideoResultScreen({ route, navigation }) {
+  const [videoStatus, setVideoStatus] = useState({});
+  const videoRef = useRef(null);
+  const { liveVideo, galleryVideo } = route.params;
+  const uri = liveVideo ? liveVideo?.uri : galleryVideo?.uri;
+
+  const saveVideo = async () => {
+    try {
+      navigation.navigate("VideoPost", { uri });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <View>
+      <Video
+        ref={videoRef}
+        style={styles.video}
+        source={{
+          uri,
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={(status) => setVideoStatus(status)}
+      />
+
+      <Button
+        title={videoStatus.isPlaying ? "Pause" : "Play"}
+        onPress={() =>
+          videoStatus.isPlaying
+            ? videoRef.current.pauseAsync()
+            : videoRef.current.playAsync()
+        }
+      />
+      <Button title="저장" onPress={saveVideo} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  video: {
+    alignSelf: "center",
+    width: "100%",
+    height: "90%",
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default VideoResultScreen;
