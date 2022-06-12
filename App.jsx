771f6115437440d1da234e6bb98a714e573444b9
LeeSyong <jejelsk27@naver.com>
2022-06-13 02:24:33 +0900

    Add: add videoConcat screen in App.jsx

--- App.jsx
+++ App.jsx
@@ -4,10 +4,11 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AuthContextProvider } from "./src/context/AuthContext";
 import "react-native-gesture-handler";
 
-import AppTabs from "./src/navigation/TabNavigation";
+import TabNavigation from "./src/navigation/TabNavigation";
 import CameraScreen from "./src/screen/CameraScreen";
 import VideoResultScreen from "./src/screen/VideoResultScreen";
 import VideoPostScreen from "./src/screen/VideoPostScreen";
+import VideoConcatScreen from "./src/screen/VideoConcatScreen";
 import LoginScreen from "./src/screen/LoginScreen";
 
 const Stack = createNativeStackNavigator();
@@ -19,7 +20,7 @@ function App() {
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen
             name="Home"
-            component={AppTabs}
+            component={TabNavigation}
             options={{ headerShown: false }}
           />
           <Stack.Screen
@@ -40,6 +41,11 @@ function App() {
           <Stack.Screen
             name="VideoPost"
             component={VideoPostScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="VideoConcat"
+            component={VideoConcatScreen}
             options={{ headerShown: false }}
           />
         </Stack.Navigator>
