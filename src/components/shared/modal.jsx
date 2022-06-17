5672b7211032a695706be161fb3aa14d276c39bf
LeeSyong <jejelsk27@naver.com>
2022-06-17 21:53:38 +0900

    Add: add modalContainer (native base modal)
    - native base modal 이용하여 재사용 모달 컴포넌트 추가

--- src/components/shared/modal.jsx
+++ src/components/shared/modal.jsx
@@ -1,41 +1,42 @@
 import React from "react";
-import { StyleSheet } from "react-native";
 import { Center, Modal, Button } from "native-base";
-import { UserAuth } from "../../context/AuthContext";
+import { ModalHandler } from "../../context/modalContext";
 
-function ModalContainer({ title, button }) {
-  const { modal, handleModal } = UserAuth();
+function ModalContainer({ navigation, needToGoBack, modalHeader, modalBody }) {
+  const { openModal, setOpenModal } = ModalHandler();
 
   return (
-    <Center style={styles.container}>
-      <Modal isOpen={modal} onClose={handleModal}>
-        <Modal.Content maxWidth="400px">
-          <Modal.CloseButton />
-          <Modal.Body>{title}</Modal.Body>
+    <Center>
+      <Modal
+        isOpen={openModal}
+        onClose={() => setOpenModal(false)}
+        _backdrop={{
+          _dark: {
+            bg: "coolGray.800",
+          },
+          bg: "blueGray.400",
+        }}
+      >
+        <Modal.Content>
+          <Modal.Header>{modalHeader}</Modal.Header>
+          <Modal.Body>{modalBody}</Modal.Body>
           <Modal.Footer>
-            <Button.Group>
-              <Button
-                variant="ghost"
-                colorScheme="blueGray"
-                onPress={handleModal}
-              >
-                {button}
-              </Button>
-            </Button.Group>
+            <Button
+              onPress={() => {
+                setOpenModal(false);
+
+                if (needToGoBack) {
+                  navigation.goBack();
+                }
+              }}
+            >
+              닫기
+            </Button>
           </Modal.Footer>
         </Modal.Content>
       </Modal>
     </Center>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 50,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default ModalContainer;
