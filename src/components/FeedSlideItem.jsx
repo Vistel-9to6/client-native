8579735281e5953f4950701e1084b531b1349fc0
JONGMIN KIM <bellmin9321@gmail.com>
2022-06-17 20:34:17 +0900

    Add: createGif & participateVideo function, add Buttons - 다른 탭으로 변경시 재생중인 비디오 종료 - GIF 다운로드, 좋아요, 참여하기 버튼 추가 - createGif & participateVideo function 추가

--- src/components/FeedSlideItem.jsx
+++ src/components/FeedSlideItem.jsx
@@ -1,10 +1,23 @@
 /* eslint-disable react/display-name */
-import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
-import { StyleSheet, View } from "react-native";
+import {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Video } from "expo-av";
+import { AntDesign } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
+import { UserAuth } from "../context/AuthContext";
 
-const PostSingle = forwardRef(({ item }, parentRef) => {
-  const ref = useRef(null);
+export const FeedSlideItem = forwardRef(({ item, navigation }, parentRef) => {
+  const videoRef = useRef(null);
+  const [like, setLike] = useState(false);
+  const { idToken, user } = UserAuth();
+  const originVideo = item?.videoUrl;
 
   useImperativeHandle(parentRef, () => ({
     play,
@@ -16,75 +29,208 @@ const PostSingle = forwardRef(({ item }, parentRef) => {
     return () => unload();
   }, []);
 
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      if (videoRef) {
+        videoRef.current.playAsync();
+      }
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("blur", () => {
+      if (videoRef) {
+        videoRef.current.pauseAsync();
+      }
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
   const play = async () => {
-    if (ref.current === null) {
+    if (videoRef.current === null) {
       return;
     }
 
-    const status = await ref.current.getStatusAsync();
+    const status = await videoRef.current.getStatusAsync();
 
     if (status?.isPlaying) {
       return;
     }
 
     try {
-      await ref.current.playAsync();
+      await videoRef.current.playAsync();
     } catch (err) {
       console.log(err);
     }
   };
 
   const stop = async () => {
-    if (ref.current === null) {
+    if (videoRef.current === null) {
       return;
     }
 
-    const status = await ref.current.getStatusAsync();
+    const status = await videoRef.current.getStatusAsync();
 
     if (!status?.isPlaying) {
       return;
     }
 
     try {
-      await ref.current.stopAsync();
+      await videoRef.current.stopAsync();
     } catch (err) {
       console.log(err);
     }
   };
 
   const unload = async () => {
-    if (ref.current === null) {
+    if (videoRef.current === null) {
       return;
     }
 
     try {
-      await ref.current.unloadAsync();
+      await videoRef.current.unloadAsync();
     } catch (err) {
       console.log(err);
     }
   };
 
+  const createGif = () => {
+    videoRef.current.pauseAsync();
+
+    if (idToken) {
+      console.log(originVideo);
+      navigation.navigate("Gif", { uri: originVideo });
+    } else {
+      navigation.navigate("Login");
+    }
+  };
+
+  const participateVideo = async () => {
+    videoRef.current.pauseAsync();
+
+    if (idToken) {
+      try {
+        navigation.navigate("Camera", { originVideo });
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      navigation.navigate("Login");
+    }
+  };
+
   return (
     <View style={styles.container}>
+      <StatusBar backgroundColor="black" style="light" />
       <Video
-        ref={ref}
+        ref={videoRef}
         style={styles.video}
         resizeMode={"cover"}
-        shouldPlay={true}
         source={{ uri: item?.videoUrl }}
+        shouldPlay={false}
+        isLooping
+        usePoster
+        posterSource={{ uri: item?.thumbnails }}
       />
+      <View style={styles.headerContainer}>
+        <View style={styles.iconBox}>
+          <>
+            <TouchableOpacity
+              onPress={createGif}
+              style={styles.downloadGifIcon}
+            >
+              <AntDesign name="download" size={24} color="white" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.downloadGifIcon}
+              onPress={() => setLike(!like)}
+            >
+              {like ? (
+                <Entypo name="heart" size={24} color="white" />
+              ) : (
+                <Entypo name="heart-outlined" size={24} color="white" />
+              )}
+            </TouchableOpacity>
+
+            <Text style={styles.gifText}>GIF</Text>
+          </>
+        </View>
+        <View style={styles.buttonBox}>
+          <TouchableOpacity onPress={participateVideo} style={styles.button}>
+            <Text style={styles.next}>
+              {originVideo ? "스토리 더하기" : "다음"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.videoInfo}>
+        <Text style={{ color: "white", fontSize: 25 }}>{item.title}</Text>
+        <Image
+          style={styles.profile}
+          source={{
+            uri: user?.profilePhoto,
+          }}
+        />
+      </View>
     </View>
   );
 });
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    backgroundColor: "black",
   },
   video: {
     width: "100%",
     height: "100%",
+    backgroundColor: "black",
+  },
+  headerContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    top: 10,
+    paddingHorizontal: 15,
+    position: "absolute",
+  },
+  iconBox: {
+    flexDirection: "row",
+  },
+  downloadGifIcon: {
+    paddingHorizontal: 15,
+  },
+  button: {
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    justifyContent: "center",
+    borderRadius: 5,
+    backgroundColor: "#FFF",
+  },
+  gifText: {
+    top: 25,
+    left: 18,
+    color: "white",
+    fontSize: 10,
+    position: "absolute",
+  },
+  videoInfo: {
+    position: "absolute",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    bottom: 10,
+    paddingHorizontal: 15,
+  },
+  profile: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "white",
   },
 });
 
-export default PostSingle;
+export default FeedSlideItem;
